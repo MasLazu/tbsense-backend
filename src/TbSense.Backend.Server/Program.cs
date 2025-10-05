fadb11be0e146ee511edf82aa79eb0a63dba4bd5
mfaziz <mfaziz50@gmail.com>
2025-10-05 23:03:12 +0700

    feat: Enhance Program.cs and project file for FastEndpoints integration and service configuration

--- src/TbSense.Backend.Server/Program.cs
+++ src/TbSense.Backend.Server/Program.cs
@@ -1,13 +1,56 @@
+using FastEndpoints;
+using FastEndpoints.Swagger;
+using MasLazu.AspNet.Framework.Application.Extensions;
+using MasLazu.AspNet.Framework.EfCore.Extensions;
+using MasLazu.AspNet.Framework.Endpoint.Extensions;
+using Microsoft.AspNetCore.Http.Json;
+using System.Text.Json;
+using TbSense.Backend.EfCore.Extensions;
+using TbSense.Backend.EfCore.Postgresql.Extensions;
+using TbSense.Backend.Endpoints.Extensions;
+using TbSense.Backend.Extensions;
 using TbSense.Backend.Server.Extensions;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Configure JSON options
+builder.Services.Configure<JsonOptions>(o =>
+    o.SerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase);
+
+// Add MasLazu.Framework
+builder.Services.AddFrameworkApplication();
+builder.Services.AddFrameworkEfCore();
+
+// Add TbSense Backend services
+builder.Services.AddTbSenseBackend(builder.Configuration);
+builder.Services.AddTbSenseBackendEndpoints();
+
+// Add EF Core with PostgreSQL
+builder.Services.AddTbSenseBackendEfCore();
+builder.Services.AddTbSenseBackendEfCorePostgresql(builder.Configuration);
+
+// Add external services (Trainer, Storage)
 builder.Services.ConfigureTrainerService(builder.Configuration);
 builder.Services.ConfigureStorageService(builder.Configuration);
+
+// Add FastEndpoints
+builder.Services.AddFastEndpoints();
+builder.Services.SwaggerDocument();
 
 WebApplication app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseHttpsRedirection();
+app.UseFrameworkExceptionHandlerMiddleware();
+app.UseFastEndpoints(c =>
+{
+    c.Serializer.Options.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+    c.Binding.UsePropertyNamingPolicy = true;
+});
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwaggerGen();
+}
 
 app.Run();
